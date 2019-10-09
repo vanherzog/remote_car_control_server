b77c1d9720e1d59a4ac2ca76c912813217eb637f
Hung Lam <hun.lam97@gmail.com>
2019-10-10 01:37:22 +0700

    chore: clean up code and camera config

--- camera.py
+++ camera.py
@@ -26,8 +26,7 @@ from ws4py.server.wsgiutils import WebSocketWSGIApplication
 WIDTH = 1024
 HEIGHT = 576
 FRAMERATE = 30
-# HTTP_PORT = 8082
-WS_PORT = 8084
+WS_PORT = 81
 COLOR = u'#444'
 BGCOLOR = u'#333'
 JSMPEG_MAGIC = b'jsmp'
@@ -36,48 +35,6 @@ VFLIP = False
 HFLIP = False
 
 ###########################################
-
-
-# class StreamingHttpHandler(BaseHTTPRequestHandler):
-#     def do_HEAD(self):
-#         self.do_GET()
-
-#     def do_GET(self):
-#         if self.path == '/':
-#             self.send_response(301)
-#             self.send_header('Location', '/index.html')
-#             self.end_headers()
-#             return
-#         elif self.path == '/jsmpg.js':
-#             content_type = 'application/javascript'
-#             content = self.server.jsmpg_content
-#         elif self.path == '/index.html':
-#             content_type = 'text/html; charset=utf-8'
-#             tpl = Template(self.server.index_template)
-#             content = tpl.safe_substitute(dict(
-#                 WS_PORT=WS_PORT, WIDTH=WIDTH, HEIGHT=HEIGHT, COLOR=COLOR,
-#                 BGCOLOR=BGCOLOR))
-#         else:
-#             self.send_error(404, 'File not found')
-#             return
-#         content = content.encode('utf-8')
-#         self.send_response(200)
-#         self.send_header('Content-Type', content_type)
-#         self.send_header('Content-Length', len(content))
-#         self.send_header('Last-Modified', self.date_time_string(time()))
-#         self.end_headers()
-#         if self.command == 'GET':
-#             self.wfile.write(content)
-
-
-# class StreamingHttpServer(HTTPServer):
-#     def __init__(self):
-#         super(StreamingHttpServer, self).__init__(
-#                 ('', HTTP_PORT), StreamingHttpHandler)
-#         with io.open('index.html', 'r') as f:
-#             self.index_template = f.read()
-#         with io.open('jsmpg.js', 'r') as f:
-#             self.jsmpg_content = f.read()
 
 
 class StreamingWebSocket(WebSocket):
@@ -134,9 +91,9 @@ def main():
     with picamera.PiCamera() as camera:
         camera.resolution = (WIDTH, HEIGHT)
         camera.framerate = FRAMERATE
-        camera.vflip = VFLIP # flips image rightside up, as needed
-        camera.hflip = HFLIP # flips image left-right, as needed
-        sleep(1) # camera warm-up time
+        camera.vflip = VFLIP  # flips image rightside up, as needed
+        camera.hflip = HFLIP  # flips image left-right, as needed
+        sleep(1)  # camera warm-up time
         print('Initializing websockets server on port %d' % WS_PORT)
         WebSocketWSGIHandler.http_version = '1.1'
         websocket_server = make_server(
@@ -146,9 +103,6 @@ def main():
             app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
         websocket_server.initialize_websockets_manager()
         websocket_thread = Thread(target=websocket_server.serve_forever)
-        # print('Initializing HTTP server on port %d' % HTTP_PORT)
-        # http_server = StreamingHttpServer()
-        # http_thread = Thread(target=http_server.serve_forever)
         print('Initializing broadcast thread')
         output = BroadcastOutput(camera)
         broadcast_thread = BroadcastThread(output.converter, websocket_server)
@@ -157,8 +111,6 @@ def main():
         try:
             print('Starting websockets thread')
             websocket_thread.start()
-            # print('Starting HTTP server thread')
-            # http_thread.start()
             print('Starting broadcast thread')
             broadcast_thread.start()
             while True:
