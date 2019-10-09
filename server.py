b28cf4f534b1db32bc3d355404677a1507548748
Raspberry Pi Server <raspberry@pi.vgu>
2019-10-10 01:01:43 +0700

    feat: camera

--- server.py
+++ server.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, request, Response, render_template, send_from_directory
+from camera import Camera
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<html><body><h1>Test site running under Flaskkkk</h1></body></html>"
+  return render_template("index.html")
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route("/test")
 def test():
-  return "<html><body><h1>Testing testing</h1></body></html>"
+  return render_template("test.html")
+
+@app.route("/stream")
+def stream():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
---
+++ testCamera.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+import io
+
+class TestCamera:
+	def __init__(self):
+		self.camera = PiCamera()
+
+	def capture(self):
+		self.camera.capture('./static/capture.jpg')
+
+	def stream(self):
+		my_stream = io.BytesIO()
+    		self.camera.start_preview()
+    		# Camera warm-up time
+    		sleep(2)
+    		self.capture(my_stream, 'jpeg')
+
+camera = TestCamera()
+camera.stream()
