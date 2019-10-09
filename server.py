b7b96cceea6e858d8f4cfbd65f9663b9d2a430dd
Hung Lam <hun.lam97@gmail.com>
2019-10-10 02:36:16 +0700

    feat: simple api

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, Response, render_template, send_from_directory
+from flask_restful import Resource, Api
+from subprocess import call
 
 app = Flask(__name__)
+api = Api(app)
+
+todos = {}
 
 @app.route("/")
 def index():
@@ -9,6 +14,22 @@ def index():
 @app.route("/test")
 def test():
   return render_template("test.html")
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return 'what'
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+class InitCamera(Resource):
+    def put(self):
+        call(['python3', 'camera.py'])
+        return 'Camera Initialized'
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(InitCamera, '/test/<string:message>')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
