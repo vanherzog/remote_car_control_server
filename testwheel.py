425ac0e1787bb5bb1c9878e131359aecc7e89f5b
Phan Le <38676528+01284071577@users.noreply.github.com>
2019-10-10 16:19:25 +0700

    feat:

--- testwheel.py
+++ testwheel.py
@@ -2,22 +2,14 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
+int1=7
+int2=11
+
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
 
-GPIO.output(7,GPIO.True)
-time.sleep(1)
-GPIO.output(7,False)
-GPIO.output(11,True)
-time.sleep(1)
-GPIO.output(11,False)
-GPIO.output(13,True)
-time.sleep(1)
-GPIO.output(13,False)
-GPIO.output(15,True)
-time.sleep(1)
-GPIO.output(15,False)
+GPIO.output(int1,GPIO.HIGH)
+GPIO.output(int2,GPIO.LOW)
+time.sleep(5)
 
 GPIO.cleanup()
