72fafcba49bf37a2ed8e617f86032fa575423ffc
Hung Lam <hun.lam97@gmail.com>
2019-10-22 15:51:18 +0700

    feat: feat: controller thread

--- control.py
+++ control.py
@@ -6,104 +6,118 @@ import time
 #int3 - 13: forward left
 #int4 - 15: backward left
 
-#wiring scheme needs to be clearer
-int1 = 7
-int2 = 11
-int3 = 13
-int4 = 15
-en1 = 16
-en2= 18
-l=0
-r=0
 
-def Setup():
-	global int1, int2, int3, int4, en1, en2, l, r
+class Controller:
+	#wiring scheme needs to be clearer
+	def __init__(self):
+		self.int1 = 7
+		self.int2 = 11
+		self.int3 = 13
+		self.int4 = 15
+		self.en1 = 16
+		self.en2= 18
+		self.l=0
+		self.r=0
+		# self.Setup();
 
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(int1,GPIO.OUT)
-	GPIO.setup(int2,GPIO.OUT)
-	GPIO.setup(int3,GPIO.OUT)
-	GPIO.setup(int4,GPIO.OUT)
-	GPIO.setup(en1,GPIO.OUT)
-	GPIO.setup(en2,GPIO.OUT)
+	def Setup(self):
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(self.int1,GPIO.OUT)
+		GPIO.setup(self.int2,GPIO.OUT)
+		GPIO.setup(self.int3,GPIO.OUT)
+		GPIO.setup(self.int4,GPIO.OUT)
+		GPIO.setup(self.en1,GPIO.OUT)
+		GPIO.setup(self.en2,GPIO.OUT)
 
-Setup()
-l = GPIO.PWM(en1, 100)
-r = GPIO.PWM(en2, 100)
-l.start(0)
-r.start(0)
-#en1,en2 one is left one is right
+	# Setup()
+	# l = GPIO.PWM(en1, 100)
+	# r = GPIO.PWM(en2, 100)
+	# l.start(0)
+	# r.start(0)
+	#en1,en2 one is left one is right
 
-#p = GPIO.PWM(channel, duty cycle)
+	#p = GPIO.PWM(channel, duty cycle)
 
-def Forward():
-	GPIO.output(int1, True)
-	GPIO.output(int2, False)
-	GPIO.output(int3, True)
-	GPIO.output(int4, False)
-def Backward():
-	GPIO.output(int1, False)
-	GPIO.output(int2, True)
-	GPIO.output(int3, False)
-	GPIO.output(int4, True)
-def Turnright():
-	GPIO.output(int1, True)
-	GPIO.output(int2, False)
-	GPIO.output(int3, False)
-	GPIO.output(int4, False)
-def Turnleft():
-	GPIO.output(int1, False)
-	GPIO.output(int2, False)
-	GPIO.output(int3, True)
-	GPIO.output(int4, False)
-def _Break_():
-	GPIO.output(int1, False)
-	GPIO.output(int2, False)
-	GPIO.output(int3, False)
-	GPIO.output(int4, False)
+	def Forward(self):
+		GPIO.output(self.int1, True)
+		GPIO.output(self.int2, False)
+		GPIO.output(self.int3, True)
+		GPIO.output(self.int4, False)
+  
+	def Backward(self):
+		GPIO.output(self.int1, False)
+		GPIO.output(self.int2, True)
+		GPIO.output(self.int3, False)
+		GPIO.output(self.int4, True)
+  
+	def Turnright(self):
+		GPIO.output(self.int1, True)
+		GPIO.output(self.int2, False)
+		GPIO.output(self.int3, False)
+		GPIO.output(self.int4, False)
+  
+	def Turnleft(self):
+		GPIO.output(self.int1, False)
+		GPIO.output(self.int2, False)
+		GPIO.output(self.int3, True)
+		GPIO.output(self.int4, False)
+  
+	def _Break_(self):
+		GPIO.output(self.int1, False)
+		GPIO.output(self.int2, False)
+		GPIO.output(self.int3, False)
+		GPIO.output(self.int4, False)
+			
+			
+	#-left +right x 
+	#+for -backward y
+	def X_axis(self):
+		if self.x < 0:
+			self.x = -self.x
+			self.l.ChangeDutyCycle(self.x*10)
+			self.Turnright()
+			print ("turning left")
+		else:
+			self.r.ChangeDutyCycle(self.x*10)
+			self.Turnleft()
+			print ("turning right")
+			
+	def Y_axis(self):
+		if self.y < 0:
+			self.y = -self.y
+			self.l.ChangeDutyCycle(self.y*20)
+			self.r.ChangeDutyCycle(self.y*20)
+			self.Backward()
+			print ("go back")
+		else:
+			self.l.ChangeDutyCycle(self.y*20)
+			self.r.ChangeDutyCycle(self.y*20)
+			self.Forward()
+			print ("go forth")
+			
+	def Control(self):
+		if self.x != 0:
+			self.X_axis(x)
+		else:
+			self.Y_axis(y)
+  
+	def set(self, x, y):
+		self.x = x
+		self.y = y
+		self.Control()
+   
+	def CleanUp(self):
+		GPIO.cleanup()
+
+
+	# while True:
+
+	# 	val1 = input("x_axis: ")
+	# 	val2 = input("y_axis: ")
+	# 	Control(val1, val2)
+	# 	time.sleep(3)
 		
-		
-#-left +right x 
-#+for -backward y
-def X_axis(x):
-	if x < 0:
-		x = -x
-		l.ChangeDutyCycle(x*10)
-		Turnright()
-		print ("turning left")
-	elif x >= 0:
-		r.ChangeDutyCycle(x*10)
-		Turnleft()
-		print ("turning right")
-		
-def Y_axis(y):
-	if y < 0:
-		y = -y
-		l.ChangeDutyCycle(y*20)
-		r.ChangeDutyCycle(y*20)
-		Backward()
-		print ("go back")
-	elif y >=0:
-		l.ChangeDutyCycle(y*20)
-		r.ChangeDutyCycle(y*20)
-		Forward()
-		print ("go forth")
-		
-def Control(x, y):
-	if x != 0:
-		X_axis(x)
-	else:
-		Y_axis(y)
-
-
-while True:
-
-	val1 = input("x_axis: ")
-	val2 = input("y_axis: ")
-	Control(val1, val2)
-	time.sleep(3)
-	
-	GPIO.cleanup()
-	Setup()
-		
-		
+	# 	GPIO.cleanup()
+	# 	Setup()
+			
+			
--- server.py
+++ server.py
@@ -5,13 +5,17 @@ from subprocess import call, check_output
 from threading import Thread
 import os
 import socket
-import control
+from control import Controller
+
 ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
 app = Flask(__name__)
 api = Api(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-todos = {}
+controller = Controller()
+connected = False
+x = 0
+y = 0
 # test commit and push
 # def listen():
 #   global socketio
@@ -24,8 +28,10 @@ todos = {}
 
 @socketio.on('message')
 def handleMessage(message):
-  
-  print('message: '+ message)
+  global x, y
+  token = message.split(',')
+  x = int(token[0])
+  y = int(token[1])
   
 @socketio.on('disconnect')
 def handleClose():
@@ -33,6 +39,8 @@ def handleClose():
   
 @socketio.on('connected')
 def handleCloseConnection():
+  global connected
+  connected = True
   print('connected')
   
 @socketio.on('motion')
@@ -62,6 +70,13 @@ class InitCamera(Resource):
     else:
       call(['sudo', 'systemctl', 'start', 'camera'])
     return(os.system('service camera status'))
+  
+def control():
+  global connected, x, y, controller
+  while connected:
+    controller.set(x, y)
+
+thread = Thread(target=control)
 
 api.add_resource(Control, '/control/<string:command>')
 api.add_resource(InitCamera, '/camera')
@@ -70,4 +85,5 @@ api.add_resource(terminate, '/terminate')
 if __name__ == "__main__":
   app.run(host=ip, debug=True)
   socketio.run(app)
+  thread.start()
   
